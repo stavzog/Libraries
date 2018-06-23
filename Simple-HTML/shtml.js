1bc253de36a6d696f1db830fc719d4a90a33c3e5
CraftOLeague <stavzog@gmail.com>
2018-06-23 18:37:18 +0300

    Create shtml.js

---
+++ Simple-HTML/shtml.js
@@ -0,0 +1,123 @@
+
+window.onerror = function(error) {
+  // do something clever here
+  console.log('Make sure that your <script></script> tag is *below* your <s-html></s-html> tag!') // do NOT do this for real!
+};
+class SHTML {
+	constructor() { 
+	}
+	create(html) {
+		/*Appends HTML (as text) to the body*/
+		document.body.innerHTML += html;
+	}
+	css(css) {
+		document.body.innerHTML += `<style>${css}</style>`
+	}
+	js(js) {
+		document.body.innerHTML += `<script>${js}</script>`
+	}
+	importjs(url) {
+		document.body.innerHTML += `<script src='${url}'></script>`
+	}
+	importcss(url) {
+		document.body.innerHTML += `<link rel='stylesheet' href='${url}'></link>`
+	}
+	select(selector) {
+		/*Selects element*/
+		var elem = {}
+		elem.selector = document.querySelector(selector);
+		elem.append = html =>{
+			/*Appends HTML (as text) to the selected element*/
+			elem.selector.innerHTML+=html;
+			return elem
+		}
+		elem.html = (nhtml=elem.selector) => {
+			/*Returns/Sets the HTML of the selected element*/
+			if (!(nhtml === elem.selector)) {
+				elem.selector.innerHTML = nhtml;
+			}
+			return elem.selector;
+
+		}
+		elem.text = (ntxt=elem.selector.textContent)=>{
+			if (!(ntxt === elem.selector.textContent)) {
+				elem.selector.textContent = ntxt;
+			}
+			return elem.selector.textContent;
+		}
+		return elem
+	}
+	build(content=`<script>var shtml = new SHTML()</script>`,title='Test',path='shtml.js') {
+		var bhtml;
+		bhtml = `
+		<html style='margin:0;padding:0;' id='shtml'>
+		<head>
+			<title>${title}</title>
+		</head>
+		<body style='margin:0;padding:0;'>
+			${content}
+			
+			<script src='${path}'></script>
+		</body>
+		</html>
+	`		
+	document.getElementsByTagName('html')[0].innerHTML = bhtml;
+	}
+
+}
+
+class SHTMLelement extends HTMLElement {
+	constructor() {
+		super();
+		this.setAttribute('style','width:100%;height:100%;background:#eee;margin:0;padding:0;')
+		let d = document.createElement('div');
+		d.setAttribute(`style`,`border-bottom:2px solid #d8d8d8;text-align:center;`);
+		d.innerHTML = `<h1 style='font-fmily:sans-serif;font-size:2em;'>Doc (Simple-HTML)</h1>`
+		this.appendChild(d);
+
+	}
+	create(html) {
+		/*Appends HTML (as text) to the body*/
+		this.innerHTML += html;
+	}
+	css(css) {
+		this.innerHTML += `<style>${css}</style>`
+	}
+	js(js) {
+		this.innerHTML += `<script>${js}</script>`
+	}
+	importjs(url) {
+		this.innerHTML += `<script src='${url}'></script>`
+	}
+	importcss(url) {
+		this.innerHTML += `<link rel='stylesheet' href='${url}'></link>`
+	}
+	select(selector='s-html') {
+		/*Selects element*/
+		var elem = {}
+		elem.selector = document.querySelector(selector);
+		elem.append = html =>{
+			/*Appends HTML (as text) to the selected element*/
+			elem.selector.innerHTML+=html;
+			return elem
+		}
+		elem.html = (nhtml=elem.selector) => {
+			/*Returns/Sets the HTML of the selected element*/
+			if (!(nhtml === elem.selector)) {
+				elem.selector.innerHTML = nhtml;
+			}
+			return elem.selector;
+
+		}
+		elem.text = (ntxt=elem.selector.textContent)=>{
+			if (!(ntxt === elem.selector.textContent)) {
+				elem.selector.textContent = ntxt;
+			}
+			return elem.selector.textContent;
+		}
+		return elem
+	}
+
+}
+
+window.customElements.define('s-html',SHTMLelement);
